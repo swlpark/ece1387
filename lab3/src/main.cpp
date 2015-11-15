f6af065a8b955dbad73bf97cab6659bbc0a21cb0
Lawrence Park <swlpark@gmail.com>
2015-11-15 13:09:19 -0500

    hey jude

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -133,18 +133,23 @@ Tree* branch_and_bound(Tree * a_node)
          a_node->right_node = nullptr;
          return nullptr;
        }
+       std::future<Tree*> r_f, l_f;
+       bool fork_r = false;
+       bool fork_l = false;
        if (Tree::thread_count < 4) {
          Tree::thread_count++;
-         auto r_f = std::async(&branch_and_bound, r_node);
-         r_recurse = r_f.get();
-       } else 
-         r_recurse = branch_and_bound(r_node);
+         r_f = std::async(&branch_and_bound, r_node);
+         fork_r = true;
+       }
        if (Tree::thread_count < 4) {
          Tree::thread_count++;
-         auto l_f = std::async(&branch_and_bound, l_node);
-         l_recurse = l_f.get();
-       } else
-         l_recurse = branch_and_bound(l_node);
+         l_f = std::async(&branch_and_bound, l_node);
+         fork_l = true;
+       }
+       if (fork_r) r_recurse = r_f.get();
+       else        r_recurse = branch_and_bound(r_node);
+       if (fork_l) l_recurse = l_f.get();
+       else        l_recurse = branch_and_bound(l_node);
      } else {
        if (l_node->getLowerBound() >= Tree::u_cut_size) {
          delete l_node; 
@@ -153,18 +158,24 @@ Tree* branch_and_bound(Tree * a_node)
          a_node->right_node = nullptr;
          return nullptr;
        }
+       std::future<Tree*> r_f, l_f;
+       bool fork_r = false;
+       bool fork_l = false;
+
        if (Tree::thread_count < 4) {
          Tree::thread_count++;
-         auto l_f = std::async(&branch_and_bound, l_node);
-         l_recurse = l_f.get();
-       } else 
-         l_recurse = branch_and_bound(l_node);
+         l_f = std::async(&branch_and_bound, l_node);
+         fork_l = true;
+       }
        if (Tree::thread_count < 4) {
          Tree::thread_count++;
-         auto r_f = std::async(&branch_and_bound, r_node);
-         r_recurse = r_f.get();
-       } else
-         r_recurse = branch_and_bound(r_node);
+         r_f = std::async(&branch_and_bound, r_node);
+         fork_r = true;
+       }
+       if (fork_l) l_recurse = l_f.get();
+       else        l_recurse = branch_and_bound(l_node);
+       if (fork_r) r_recurse = r_f.get();
+       else        r_recurse = branch_and_bound(r_node);
      }
      if (l_recurse != nullptr && r_recurse != nullptr) {
         if (r_recurse->cut_size < l_recurse-> cut_size) {
